1be733c679eb5849bd5d9788bcdea6cf370eb735
shabrul <shabrul2451@gmail.com>
2022-01-27 11:58:25 +0600

    minor changes

--- api/v1/base.go
+++ api/v1/base.go
@@ -34,45 +34,44 @@ func GithubEventRouter(g *echo.Group) {
 
 // ApplicationRouter api/v1/applications/* router
 func ApplicationRouter(g *echo.Group) {
-	applicationApi := NewApplicationApi(dependency.GetV1CompanyService())
+	applicationApi := NewApplicationApi(dependency.GetV1CompanyService(), dependency.GetV1JwtService())
 	g.POST("", applicationApi.Update)
 }
 
 // RepositoryRouter api/v1/repositories/* router
 func RepositoryRouter(g *echo.Group) {
-	repositoryApi := NewRepositoryApi(dependency.GetV1CompanyService())
+	repositoryApi := NewRepositoryApi(dependency.GetV1CompanyService(), dependency.GetV1JwtService())
 	g.GET("/:id", repositoryApi.GetById)
 	g.GET("/:id/applications", repositoryApi.GetApplicationsById)
 }
 
 // PipelineRouter api/v1/pipelines/* router
 func PipelineRouter(g *echo.Group) {
-	pipelineApi := NewPipelineApi(dependency.GetV1PipelineService())
+	pipelineApi := NewPipelineApi(dependency.GetV1PipelineService(), dependency.GetV1JwtService())
 	g.GET("/:id", pipelineApi.GetLogs)
 	g.GET("/ws", pipelineApi.GetEvents)
 }
 
 // ProcessRouter api/v1/processes/* router
 func ProcessRouter(g *echo.Group) {
-	processApi := NewProcessApi(dependency.GetV1ProcessService())
+	processApi := NewProcessApi(dependency.GetV1ProcessService(), dependency.GetV1JwtService())
 	g.GET("", processApi.GetByCompanyIdAndRepositoryIdAndAppId)
 
 }
 
 // CompanyRouter api/v1/companies/* router
 func CompanyRouter(g *echo.Group) {
-	companyApi := NewCompanyApi(dependency.GetV1CompanyService(),dependency.GetV1JwtService())
-	g.POST("", companyApi.Save,AuthenticationHandler)
+	companyApi := NewCompanyApi(dependency.GetV1CompanyService(), dependency.GetV1JwtService())
+	g.POST("", companyApi.Save, AuthenticationHandler)
 	g.GET("", companyApi.GetCompanies)
 	g.GET("/:id", companyApi.GetById)
 	g.GET("/:id/repositories", companyApi.GetRepositoriesById)
 	g.PUT("/:id/repositories", companyApi.UpdateRepositories)
 }
 
-
 // ProcessLifeCycleEventRouter api/v1/process_life_cycle_events/* router
 func ProcessLifeCycleEventRouter(g *echo.Group) {
-	processLifeCycleEventApi := NewProcessLifeCycleEventApi(dependency.GetV1ProcessLifeCycleEventService())
+	processLifeCycleEventApi := NewProcessLifeCycleEventApi(dependency.GetV1ProcessLifeCycleEventService(), dependency.GetV1JwtService())
 	g.POST("", processLifeCycleEventApi.Save)
 	g.GET("", processLifeCycleEventApi.Pull)
 }
