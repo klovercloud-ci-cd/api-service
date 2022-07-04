b51473bcc9645ab5ad20da8a34be9aacd39ebdc1
niloydeb1 <niloymishu@gmail.com>
2022-07-04 12:49:05 +0600

    Refactor get pipeline by id api

--- core/v1/service/pipeline.go
+++ core/v1/service/pipeline.go
@@ -4,7 +4,7 @@ import v1 "github.com/klovercloud-ci-cd/api-service/core/v1"
 
 // Pipeline Pipeline operations.
 type Pipeline interface {
-	Get(companyId, repositoryId, url, revision, action, from, to string) (httpCode int, body interface{})
+	Get(companyId, repositoryId, applicationId, url, revision, action, from, to string) (httpCode int, body interface{})
 	GetByProcessId(companyId, processId, action string, option v1.Pagination) (httpCode int, body interface{})
 	ReadEventsByCompanyId(c chan map[string]interface{}, companyId, userId string)
 	ReadEventsByCompanyIdAndUserIdAndTime(c chan map[string]interface{}, companyId, userId, from string)
